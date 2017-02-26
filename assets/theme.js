b677ceadcf4551398ccffd97d3e55a60f7ff962c
Lisa Yoder <lisa.alane.yoder@gmail.com>
2017-02-26 13:24:16 -0500

    some updates for menu plugin

--- assets/theme.js
+++ assets/theme.js
@@ -7,12 +7,17 @@
 
 
 jQuery(document).ready(function(jQuery){
-	jQuery( ".hamburger" ).click(
+	jQuery( ".mega-hamburger" ).click(
 	  function() {
-	  	// console.log("hi");
+	  	console.log("hi");
 	    // jQuery( ".current-menu-ancestor ul.submenu" ).show;
 	    jQuery( ".current-menu-ancestor ul.sub-menu" ).toggle();
 	    jQuery( ".current-menu-item ul.sub-menu" ).toggle();
+	    jQuery( ".mega-current-menu-ancestor ul.mega-sub-menu" ).toggle();
+	    jQuery( ".mega-current-menu-item ul.mega-sub-menu" ).toggle();
+	    jQuery( ".mega-current-menu-ancestor ul.mega-sub-menu" ).css({"visibility" : "visible"});
+	    jQuery( ".mega-current-menu-item ul.mega-sub-menu" ).css({"visibility" : "visible"});
 	  }
 	);
+
 });
