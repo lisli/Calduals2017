8258d95bb340a6ce3fd471bfaedf463a25ce785b
Lisa Yoder <lisa.alane.yoder@gmail.com>
2017-03-31 16:36:04 -0400

    add js to open pdfs and docs in new tab

--- assets/theme.js
+++ assets/theme.js
@@ -17,21 +17,25 @@ jQuery(document).ready(function(jQuery){
 	//  }
 	// );
 
-	// The '#theLink' portion is a selector which matches a DOM element
-	// with the id 'theLink' and .click registers a call back for the 
-	// element being clicked on 
+	//Display popup for external links; open pdfs and other documents in new tab
 
 	jQuery(document).on("click", "a", function(event) {
 
 		var internalLink = /(calduals|^#|^mailto:)/; //regex to look for 'calduals' or anything that starts with '#' to account for jump links
 		var link = jQuery(this).attr("href");
 		var isExternal = (!link.match(internalLink));
+		var isDocument = \.pdf|\.doc|\.docx\ //regex to look for pdfs or other documents that may open
+
 		if (isExternal) {
 			event.preventDefault();
 			var answer = confirm("You are now leaving CalDuals.org, the Coordinated Care Initiative’s website. When you leave this website to access a different site, you become subject to the other website’s privacy policy and practices."); 
 			if (answer) {
 				window.open(link, '_blank');
 			}
+		}
+
+		if(isDocument) {
+			window.open(link, '_blank');
 		}
   	});
 
